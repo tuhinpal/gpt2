343f79945b417d008fea7a9bd9115714a9683756
tuhinpal <tkp03463@gmail.com>
2022-05-01 16:58:28 +0000

    Comment out debug=True

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def index():
         }
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=port, use_reloader=True, threaded=True)
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=port, use_reloader=True, threaded=True)
 
 # Production
 if __name__ == '__main__':
